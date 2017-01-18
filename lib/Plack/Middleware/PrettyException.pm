b1835a3aaf5381f8ce4b4d03b87f9e65725f01c7
Thomas Klausner <domm@plix.at>
2017-01-18 11:35:42 +0100

    bumped version, changes

--- lib/Plack/Middleware/PrettyException.pm
+++ lib/Plack/Middleware/PrettyException.pm
@@ -2,7 +2,7 @@ package Plack::Middleware::PrettyException;
 
 # ABSTRACT: Capture exceptions and present them as HTML or JSON
 
-our $VERSION = '1.000';
+our $VERSION = '1.001';
 
 use 5.010;
 use strict;
